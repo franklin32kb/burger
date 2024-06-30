7578d20105a70771e93e0d8a9ffbf092045af7d0
Franklin camacho <franklin32kb@gmail.com>
2024-06-30 17:35:39 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+  this.classList.toggle('active');
+  document.querySelector('.nav-links').classList.toggle('active');
+});
+
+// Carousel functionality can be added here if needed
